1edce6208fa9ea6b232a1a407ccbaac5384e4061
Anand Suresh <wwwanandsuresh@gmail.com>
2019-05-16 13:38:08 +0530

    replace the current run.py with this

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from flask import Flask, flash, redirect, render_template, url_for, request
 from flask_cors import CORS
-import assistant
+#import assistant
 import json
 
 app = Flask(__name__)
@@ -12,9 +12,10 @@ CORS(app)# to let the webapp know that this backend is ready to accept stuff.
 def get_names():
    if request.method == 'POST':
        resp_json = request.get_json()
+       print(resp_json['text'])
        
-       response = assistant.assistant(resp_json["test"])			
-       return json.dumps({"response": response}), 200
+       #response = assistant.assistant(resp_json["test"])
+       return json.dumps({"response": resp_json['text']}), 200
 
 if __name__=='__main__':
     app.run(debug=True)
